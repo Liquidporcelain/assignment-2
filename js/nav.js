be5434a891fd896840d6a634c143c99296ba985a
Liquidporcelain <colin_o_o@hotmail.com>
2014-01-06 10:24:19 -0500

    added content

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,18 @@
-// JavaScript Document
+// Toggle Nav JS
 
 	var navTop = document.querySelector('.nav-top');
 	
 	document.querySelector('.nav-btn').addEventListener('click', function(e){
-	e.preventDefault();
+		e.preventDefault(); 
+	
+	
+		if (navTop.getAttribute('data-state') == 'expanded'){
+			
+			navTop.setAttribute('data-state', 'collapsed');
+		  this.setAttribute('data-state', 'inactive');
+		}else{
 		
-	if(navTop.getAttribute('data-state') == 'expanded'){
-		navTop.setAttribute('data-state', 'collapsed');
-	}else{
-		navTop.setAttribute('data-state', 'expanded');
-	}
-	
-	
-	
-	
+			navTop.setAttribute('data-state', 'expanded');
+	    this.setAttribute('data-state', 'active');
+		}
 	});
-	
